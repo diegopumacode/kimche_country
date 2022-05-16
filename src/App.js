64b6205dfe12b6ab6ee3e16bbc5f35d176a07714
diegopuma <diegopumacode@gmail.com>
2022-05-15 23:16:37 -0500

    feat(seacrh country by name): adding country search by name, reducer and search logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
 import Wrapper from './components/Wrapper';
+import SearchCountry from './features/searchCountry';
 
 function App() {
   return (
     <Wrapper>
       <Header
-        title="Country Search"
+        title="🌎 Country Search"
         subtitle="Search by country: here you will get an overview of countries by continent and languages"
       />
+      <SearchCountry />
     </Wrapper>
   );
 }
---
+++ src/components/Alert.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Alert({ message, altText = '', src }) {
+  return (
+    <StyledAlert>
+      <img src={src} alt={altText} width={200} />
+      <p>{message}</p>
+    </StyledAlert>
+  );
+}
+
+const StyledAlert = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: .5rem;
+  padding: 3rem 0;
+  img{
+    margin-bottom: 1rem;
+  }
+  p{
+    font-size: 1rem;
+    
+    text-align: center;
+    min-width: 200px;
+  }
+
+`;
